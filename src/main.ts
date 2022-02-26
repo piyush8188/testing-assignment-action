cf96201fefe96ef69ba7e205254421750a4e03ca
Subhra264 <chakrabortysubhradeep556@gmail.com>
2022-02-26 13:17:21 +0530

    Integrate cypress, make API request to calculate the score

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,63 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import fs from 'fs';
+import path from 'path';
+import axios from 'axios';
+
+// acciotest.json
+/*
+{
+  'testRepo': string',
+  'pathToFile': 'string'
+}
+*/
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const repoName = process.env['GITHUB_REPOSITORY']?.split('/')[1];
+    if (!repoName) throw new Error('Failed to parse repoName');
+    
+    const repoWords = repoName?.split('-');
+    const studentUserName = repoWords[repoWords.length - 1];
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const repoWorkSpace: string | undefined = process.env['GITHUB_WORKSPACE'];
+    if (!repoWorkSpace) throw new Error('No GITHUB_WORKSPACE');
 
-    core.setOutput('time', new Date().toTimeString())
+    const accioTestConfigData = fs.readFileSync(path.resolve(repoWorkSpace, 'acciotest.json'));
+    const accioTestConfig = JSON.parse(accioTestConfigData.toString());
+
+    const query = new URLSearchParams();
+    query.append('repo', accioTestConfig.testRepo);
+    query.append('filePath', accioTestConfig.pathToFile);
+    
+    // Get the encoded test file contents
+    const encodedTestFileData = await axios.get('http://localhost:5001/github/action-get-file?' + query.toString());
+    const testFileContent = Buffer.from(encodedTestFileData.data, 'base64').toString('utf8');
+
+    fs.writeFileSync(path.resolve(repoWorkSpace, 'tests/test.ts'), testFileContent);
+
+    await exec.exec('npm install cypress', undefined, {
+      cwd: repoWorkSpace
+    });
+    
+    const cypressPath = require.resolve('cypress', {
+      paths: [repoWorkSpace]
+    }) || 'cypress';
+
+    const cypress = require(cypressPath);
+    const testResults = await cypress.run();
+    
+    const { data: score } = await axios.post('http://localhost:5001/github/action-get-file', {
+      testResults,
+      studentGituhbUserName: studentUserName
+    });
+
+    core.setOutput('totalScore', score.totalScore);
+    core.setOutput('scoreReceived', score.scoreReceived);
+
+    process.stdout.write(`\nScore: ${score.scoreReceived}/${score.totalScore}\n`);
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) core.setFailed(error.message);
   }
 }
 
